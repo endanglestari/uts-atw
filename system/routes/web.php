8e4dc9c6b25106107660fe33afe5cc5a4f807079
endanglestari <71061298+endanglestari@users.noreply.github.com>
2020-11-29 18:54:48 +0700

    Initial commit

---
+++ system/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('beranda', [HomeController::class, 'showBeranda']);
+Route::get('kontak', [HomeController::class, 'showKontak']);
+
+	Route::resource('produk', ProdukController::class);
+	Route::resource('kategori', KategoriController::class);
+	Route::resource('user', UserController::class);
+	Route::resource('register', RegisterController::class);
+});
+
+
+Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('login', [AuthController::class, 'loginProcess']);
+Route::get('logout', [AuthController::class, 'logout']);
